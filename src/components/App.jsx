17a7816006af9ca67bd464832e54afc93b171e1d
Agustin Ecker <amecker@hotmail.com>
2022-01-30 18:03:01 -0300

    eslint, created skeleton with container for navbar and home

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Home from './home/Home';
+import Nav from './nav/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
