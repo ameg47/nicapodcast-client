528ed2af8266a4b2aedfa9d91f2ac0cef194d2b0
Agustin Ecker <amecker@hotmail.com>
2022-01-31 17:03:32 -0300

    added footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Footer from './footer/Footer';
 import Home from './home/Home-container';
 import Nav from './nav/Nav';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="font-poppins">
       <Nav />
       <Home />
+      <Footer />
     </div>
   );
 }
