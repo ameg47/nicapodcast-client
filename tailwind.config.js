1d9a35333f5c2b9fd44bda4900d77c09e294d2de
Agustin Ecker <amecker@hotmail.com>
2022-01-31 10:21:13 -0300

    added elements to navbar

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        myred: '#F13C45',
+        'Dark-blue': '#0B2342',
+      },
+    },
+  },
+  plugins: [],
+};
